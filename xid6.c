6e51594e2b42a9f8a8047b920b84327c27e044c2
Anosh Ullenius <anosh@anosh.se>
2023-07-28 14:53:38 +0200

    Add missing fields. Use length in sub-chunk header

--- xid6.c
+++ xid6.c
@@ -8,24 +8,27 @@
 #define XID6_HEADER_MAGIC   ("xid6")
 #define XID6_HEADER_LENGTH  8 // magic + length
 #define XID6_OFFSET         0x10200
-#define FADE_LENGTH         4
 
 struct xid6 {
-    uint16_t copyright_year;
-    uint32_t intro_length;
-    uint32_t fade_length;
-    uint8_t number_of_times_to_loop;
-    uint16_t ost_track;
-    char *song;
-    char *game;
-    char *artist;
-    char *dumper;
-    uint32_t dumped_date;
-    uint8_t emulator;
-    char *comments;
-    char *ost_title; // official soundtrack title
-    uint8_t ost_disc;
-    char *publishers_name;
+    char        *song;
+    char        *game;
+    char        *artist;
+    char        *dumper;
+    uint32_t    dumped_date;
+    uint8_t     emulator;
+    char        *comments;
+    char        *ost_title; // official soundtrack title
+    uint8_t     ost_disc;
+    uint16_t    ost_track;
+    char        *publishers_name;
+    uint16_t    copyright_year;
+    uint32_t    intro_length;
+    uint32_t    loop_length;
+    uint32_t    end_length;
+    uint32_t    fade_length;
+    uint8_t     muted_voices;
+    uint8_t     number_of_times_to_loop;
+    uint32_t    mixing_level;
 };
 
 uint32_t parse_u32(const uint8_t*);
@@ -54,16 +57,28 @@ void *allocate_copy (const uint8_t *src, size_t len) {
     return buf;
 }
 
-void set_intro_length( struct xid6 *tags, const uint8_t *src) {
+void set_intro_length( struct xid6 *tags, const uint8_t *src ) {
     tags->intro_length = parse_u32( src );
 }
 
-void set_fade_length( struct xid6 *tags, const uint8_t *src) {
+void set_fade_length( struct xid6 *tags, const uint8_t *src ) {
     tags->fade_length = parse_u32( src );
 }
 
-void set_dumped_date( struct xid6 *tags, const uint8_t *src) {
+void set_dumped_date( struct xid6 *tags, const uint8_t *src ) {
     tags->dumped_date = parse_u32( src );
+}
+
+void set_loop_length( struct xid6 *tags, const uint8_t *src ) {
+    tags->loop_length = parse_u32( src );
+}
+
+void set_end_length( struct xid6 *tags, const uint8_t *src ) {
+    tags->end_length = parse_u32( src );
+}
+
+void set_mixing_level( struct xid6 *tags, const uint8_t *src ) {
+    tags->mixing_level = parse_u32( src );
 }
 
 inline uint32_t parse_u32(const uint8_t *src) {
@@ -90,23 +105,17 @@ void parse_xid6( struct binary_file *spc ) {
 
         switch (id) {
             case 0x1:
-                tags.song = allocate_copy( &spc->data[ offset ], val );
-                break;
+                tags.song = allocate_copy( &spc->data[ offset ], val ); break;
             case 0x2:
-                tags.game = allocate_copy( &spc->data[ offset ], val );
-                break;
+                tags.game = allocate_copy( &spc->data[ offset ], val ); break;
             case 0x3:
-                tags.artist = allocate_copy( &spc->data[ offset ], val );
-                break;
+                tags.artist = allocate_copy( &spc->data[ offset ], val ); break;
             case 0x4:
-                tags.dumper = allocate_copy( &spc->data[ offset ], val );
-                break;
+                tags.dumper = allocate_copy( &spc->data[ offset ], val ); break;
             case 0x5:
-                set_dumped_date( &tags, &spc->data[ offset ] );
-                break;
+                set_dumped_date( &tags, &spc->data[ offset ] ); break;
             case 0x6:
-                tags.emulator = val & 0xFF;
-                break;
+                tags.emulator = val & 0xFF; break;
             case 0x7:
                 tags.comments = allocate_copy( &spc->data[ offset ], val );
                 break;
@@ -114,27 +123,28 @@ void parse_xid6( struct binary_file *spc ) {
                 tags.ost_title = allocate_copy( &spc->data[ offset ], val );
                 break;
             case 0x11:
-                tags.ost_disc = val;
-                break;
+                tags.ost_disc = val; break;
             case 0x12:
-                tags.ost_track = val;
-                break;
+                tags.ost_track = val; break;
             case 0x13:
                 tags.publishers_name = allocate_copy( &spc->data[ offset ], val ); 
                 break;
             case 0x14:
-                tags.copyright_year = val;
-                break;
+                tags.copyright_year = val; break;
             case 0x30:
-                set_intro_length( &tags, &spc->data[ offset ] );
-                break;
+                set_intro_length( &tags, &spc->data[ offset ] ); break;
+            case 0x31:
+                set_loop_length( &tags, &spc->data[ offset ] ); break;
+            case 0x32:
+                set_end_length( &tags, &spc->data[ offset ] ); break;
             case 0x33:
-                set_fade_length( &tags, &spc->data[ offset ] );
-                offset += FADE_LENGTH;
-                break;
+                set_fade_length( &tags, &spc->data[ offset ] ); break;
+            case 0x34:
+                tags.muted_voices = val & 0xFF; break;
             case 0x35:
-                tags.number_of_times_to_loop = val;
-                break;
+                tags.number_of_times_to_loop = val & 0xFF; break;
+            case 0x36:
+                set_mixing_level( &tags, &spc->data[ offset ] ); break;
             default:
                 fprintf(stderr, "Unknown id: %#x\n", id);
                 exit(-1);
@@ -143,7 +153,7 @@ void parse_xid6( struct binary_file *spc ) {
         if ( type == 0 ) {
             printf("val stored in sub-chunk header: %d\n", val);
         } else {
-            printf("increase offset by: %d\n", val);
+            printf("increase offset by: %d\n", val); // TODO use hard-coded ?
             // 4-byte alignment of data
             uint8_t padding = (4 - (val % 4) ) % 4; // sub-chunk header is 4 bytes
             if (padding) {
